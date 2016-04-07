71b89661a8136b65546f0988c243cf714f9a8d3e
Bin Zhang <30135878@qq.com>
2016-04-07 19:24:41 +0800

    iOS正常打开web view不出错。

--- ZBAnt/index.ios.js
+++ ZBAnt/index.ios.js
@@ -8,45 +8,87 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  WebView
 } from 'react-native';
 
-class ZBAnt extends Component {
-  render() {
+var HEADER = '#3b5998';
+var BGWASH = 'rgba(255,255,255,0.8)';
+var DISABLED_WASH = 'rgba(255,255,255,0.25)';
+
+var TEXT_INPUT_REF = 'urlInput';
+var WEBVIEW_REF = 'webview';
+var DEFAULT_URL = 'http://baidu.com';
+
+var WebViewExample = React.createClass({
+
+  getInitialState: function() {
+    return {
+      url: DEFAULT_URL,
+      status: 'No Page Loaded',
+      backButtonEnabled: false,
+      forwardButtonEnabled: false,
+      loading: true,
+      scalesPageToFit: true,
+    };
+  },
+
+  inputText: '',
+
+  render: function() {
+    this.inputText = this.state.url;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={[styles.container]}>
+        <WebView
+          ref={WEBVIEW_REF}
+          automaticallyAdjustContentInsets={false}
+          style={styles.webView}
+          source={{uri: this.state.url}}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          decelerationRate="normal"
+          onNavigationStateChange={this.onNavigationStateChange}
+          onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest}
+          startInLoadingState={true}
+          scalesPageToFit={this.state.scalesPageToFit}
+        />
       </View>
     );
-  }
-}
+  },
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  onShouldStartLoadWithRequest: function(event) {
+    // Implement any custom loading logic here, don't forget to return!
+    return true;
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  onNavigationStateChange: function(navState) {
+    this.setState({
+      backButtonEnabled: navState.canGoBack,
+      forwardButtonEnabled: navState.canGoForward,
+      url: navState.url,
+      status: navState.title,
+      loading: navState.loading,
+      scalesPageToFit: true
+    });
   },
 });
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: HEADER,
+  },
+  webView: {
+    backgroundColor: BGWASH,
+    height: 350,
+  },
+});
+
+class ZBAnt extends Component {
+    render(): ReactElement { return <WebViewExample />; }
+}
 
 AppRegistry.registerComponent('ZBAnt', () => ZBAnt);
